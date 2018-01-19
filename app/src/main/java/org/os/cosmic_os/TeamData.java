9de4f1a7bacd7bdb6cc82e2168f407d80fc21383
zeromod <zeromoddev@gmail.com>
2018-01-19 14:09:10 +0530

    [V2.0] Load Team Data from repo to avoid rebuilding apk and pushing to source

--- app/src/main/java/org/os/cosmic_os/TeamData.java
+++ app/src/main/java/org/os/cosmic_os/TeamData.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 
@@ -44,14 +46,18 @@ class TeamData {
         String json = null;
 
         try {
-            InputStream is = context.getAssets().open("team.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            int read = is.read(buffer);
-            if (read >= 0) {
-                is.close();
-                json = new String(buffer, "UTF-8");
-            }
+            InputStream inputStream;
+            String filePath = context.getExternalFilesDir(null) + "/" + "team.json";
+            File file = new File(filePath);
+            if (file.exists()) inputStream = new FileInputStream(file);
+            else inputStream = context.getAssets().open("team.json");
+                int size = inputStream.available();
+                byte[] buffer = new byte[size];
+                int read = inputStream.read(buffer);
+                if (read >= 0) {
+                    inputStream.close();
+                    json = new String(buffer, "UTF-8");
+                }
         }
         catch (java.io.IOException ex) {
             ex.printStackTrace();
